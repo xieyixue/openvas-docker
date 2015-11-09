ffeb771d0f28f12543d51deae091172982c504e7
Mike Splain <mike.splain@gmail.com>
2015-11-09 09:18:53 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 docker run -d -p 443:443 -p 9390:9390 -p 9391:9391 --name openvas openvas
 
 echo "Waiting for startup to complete..."
-until docker logs --tail=15 openvas | grep -E 'It seems like your OpenVAS-8 installation is'; do
+until docker logs --tail=15 openvas | grep -E 'It seems like your OpenVAS-9 installation is'; do
   echo "." ;
   echo "=========================================================================";
   docker logs --tail=15 openvas
@@ -11,4 +11,4 @@ until docker logs --tail=15 openvas | grep -E 'It seems like your OpenVAS-8 inst
   sleep 5 ;
 done
 
-docker logs --tail=15 openvas | grep -E 'It seems like your OpenVAS-8 installation is OK'
+docker logs --tail=15 openvas | grep -E 'It seems like your OpenVAS-9 installation is OK'
